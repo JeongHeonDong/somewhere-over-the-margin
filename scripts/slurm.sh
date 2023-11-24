57dc670ae2ca13f8ec56f5ff538eb15060f9f5a4
alterkim <alter@kakao.com>
2023-11-24 17:17:34 +0900

    FIX: Slum script more specified

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -1,15 +1,46 @@
 #!/bin/sh
 
 #SBATCH -J CV_Project
-#SBATCH -p titanxp
-#SBATCH -N 1
-#SBATCH -n 1
 #SBATCH -o test.out
 #SBATCH -e test.err
+#SBATCH -t 1-00:00:00
+
+#### Select GPU
+#SBATCH -p titanxp
 #SBATCH --gres=gpu:2
+
+#### Select node (change nodelist at your circumstances)
+#SBATCH --nodelist=n1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+cd  $SLURM_SUBMIT_DIR
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
 
 module purge
 module load cuda/10.2 conda
+
+echo "Start"
+
+echo "conda activate cv-project"
 conda activate cv-project
 
-srun python -m src.examples.tripletMarginLossMNIST
+SAMPLES_DIR=$HOME/somewhere-over-the-margin/
+python3 $SAMPLES_DIR/src/examples/tripletMarginLossMNIST.py
+
+date
+
+echo "conda deactivate cv-project"
+conda deactivate
+
+squeue --job $SLURM_JOBID
+
+echo "##### END #####"
